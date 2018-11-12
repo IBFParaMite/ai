3c114824649e649c716bbf959dede0949f2c7063
Cameron Ferns <cameron@chrisferns.co.uk>
2018-11-12 23:13:17 +0000

    General changes
    Changed AI.Core and AI.Class_Library to Console App projects rather than Windows Universal
    Added Bot_Functions.cs for all the functions to do with the bot itself
    Added Relationships.cs for all the functions to do with the relationship the user has with the bot
    Removed Class1.cs
    Removed Stories.cs
    Added Bot.cs for defining the bot
    Added User.cs for defining the user
    Added the ability to check a user's VIP status in Bot_Functions
    Added basic functionality in Program.cs

---
+++ AI.V2/AI.Class_Library/Bot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Class_Library
+{
+    class Bot
+    {
+        public string BotName { get; set; }
+
+        public List<string> BotRelationships = new List<string>()
+        {
+            "Married",
+            "Dating",
+            "Friends",
+            "Colleagues",
+            "Associates",
+            "Enemies"
+        };
+    }
+}
---
+++ AI.V2/AI.Class_Library/Relationships.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Class_Library
+{
+    public class AskOut
+    {
+        public static void Response()
+        {
+            string[] positiveresponses = new string[]
+            {
+                "Of course I will!",
+                "Are you kidding me? Yes!",
+                "I've been waiting for you to ask me for a long time! Yes!",
+                ""
+            };
+
+            string[] negativeresponses = new string[]
+            {
+                "I don't know if I feel the same way",
+                "We don't really know each other!",
+                "I don't think so",
+            };
+
+            Random rand = new Random();
+            int responseCount = rand.Next(positiveresponses.Length);
+            var botresponse = positiveresponses[responseCount];
+
+            Console.WriteLine(botresponse);
+        }
+    }
+
+    public class Date
+    {
+        public static void DateScenarios()
+        {
+
+        }
+    }
+}
